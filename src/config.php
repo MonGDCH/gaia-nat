5d350c701fc8739915a1daf6818642753808c198
monLam <985558837@qq.com>
2025-12-12 17:50:43 +0800

    增强配置选项

--- src/config.php
+++ src/config.php
@@ -6,14 +6,14 @@ return [
     'debug'         => env('NAT_DEBUG', false),
     // 内网穿透服务鉴权token
     'token'         => env('NAT_TOKEN', '%s'),
-    // 服务端地址
+    // 服务端channel通信服务地址
     'channel_host'  => env('NAT_CHANNEL_HOST', 'nat.gdmon.com'),
     // 服务端channel通信服务端口
     'channel_port'  => env('NAT_CHANNEL_PORT', 2209),
     // 本地内网穿透地址
-    'proxy_host'    => '127.0.0.1',
+    'proxy_host'    => env('NAT_PROXY_HOST', '127.0.0.1'),
     // 本地内网穿透端口，映射本地80端口，通过配置host和Nginx多域名，实现完全的内网透传
-    'proxy_port'    => 80,
+    'proxy_port'    => env('NAT_PROXY_PORT', 80),
     // 事件标识名称
     'event'         => [
         // 客户端注册事件标识
@@ -35,8 +35,8 @@ return [
     'server'    => [
         // 监听协议端口，需要对外开放，外网访问对应的IP端口即可，tcp协议即支持http、https、tcp三种穿透方式
         // 也可以监听本地IP，通过nginx代理，防止被外部访问
-        'listen'    => 'tcp://127.0.0.1:' . env('NAT_NET_PORT', 8001),
+        'listen'    => 'tcp://127.0.0.1:' . env('NAT_SERVER_PORT', 8087),
         // 进程数，代理服务端一般单个进程即可
-        'count'     => env('NAT_NET_COUNT', 1),
+        'count'     => env('NAT_SERVER_COUNT', 1),
     ]
 ];
